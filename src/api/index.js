03f4e0158906a6b5be19731763427182688333e8
Sishnenko Evgeniy <sishnenko86@mail.ru>
2020-09-14 22:21:12 +0300

    com

--- src/api/index.js
+++ src/api/index.js
@@ -3,9 +3,7 @@ export const searchNews = async (search) => {
   // const response = await fetch(`${process.env.REACT_APP_SEARCH_URL}?${params}`);
   // console.log("dfgdsf");
 
-  const response = await fetch(`http://localhost:7070/api/news`, {
-    method: "GET",
-  });
+  const response = await fetch(`http://localhost:7070/api/news`);
   if (!response.ok) {
     throw new Error(response.statusText);
   }
